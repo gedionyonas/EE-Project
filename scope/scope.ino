329b24bcfdc4d5daaed762e1951f35ec82cb44c7
Gedion Y. Metaferia <gedionyonas234@gmail.com>
2014-04-29 03:16:15 -0400

    Completed the broadcast sketch. Reformated the toggle switch class in standard form. I've placed it in the libraries folder.

--- scope/scope.ino
+++ scope/scope.ino
@@ -1,4 +1,21 @@
+/*
+  The basic function of this sketch is to read
+  inputs and broadcast them through the serial port.
+  A python program will pick up the signal and use it
+  to draw the input. 
+*/
+#include <ToggleSwitch.h>
 
+int channel_1 = A0; // scope channels
+int channel_2 = A1;
+
+int vertical = 5; //sensitivity controls
+int horizontal = 6;
+
+//toggle switches
+ToggleSwitch ch1_toggle(13);  //turns channel 1 on and off
+ToggleSwitch ch2_toggle(12);  //turns channel 2 on and off
+ToggleSwitch trigger(11);     //switches 'trigger' on and off
 void setup(){
   
   Serial.begin(9600);
@@ -13,7 +30,7 @@ void loop(){
   Defines push buttons and adds debounce functionality.
   Inspired by: http://www.arduino.cc/en/Tutorial/Switch#
 */
-
+/*
 class ToggleSwitch{
   public:
     ToggleSwitch(int in_pin); //constructor
@@ -48,4 +65,4 @@ int ToggleSwitch :: read_state(){
    previous = current;
    return state;
  
-} 
+} */
